124afa6b99ee163930b232ff54f8d412fd47d791
Shamiltestgrid <60189855+Shamiltestgrid@users.noreply.github.com>
2020-09-03 10:46:50 +0300

    Update Main11.java

--- Main11.java
+++ Main11.java
@@ -1,10 +1,10 @@
-public class Main1 {
+public class Main11 {
   public static void main(String[] args) {
    
     int amount = 1500;
-    int transfercash = 20;
+    int transferCash = 20;
 
-    int bonus = amount / transfercash;
+    int bonus = amount / transferCash;
 
     System.out.println(bonus);
   }
